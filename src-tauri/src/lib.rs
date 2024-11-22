395fa9dd7e58596734419d2cfa37a469a13e814e
William Cory <willcory10@gmail.com>
2024-11-22 08:27:32 -0800

    implement eth_getBlock

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,16 @@
-mod client;
+use tokio::sync::Mutex;
 
-use std::sync::Mutex;
+use alloy::rpc::types::Transaction;
+use helios::core::types::{Block, BlockTag};
+use helios::ethereum::{
+    config::networks::Network, database::FileDB, EthereumClient, EthereumClientBuilder,
+};
+use std::path::PathBuf;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .manage(Mutex::new(None::<client::LightClient>))
+    .manage(Mutex::new(AppState::default()))
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -18,20 +23,27 @@ pub fn run() {
     })
     .invoke_handler(tauri::generate_handler![
       start,
+      get_block,
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
 #[tauri::command]
-async fn start(state: tauri::State<'_, Mutex<Option<client::LightClient>>>) -> Result<String, String> {
+async fn start(state: tauri::State<'_, Mutex<AppState>>) -> Result<String, String> {
     let mut client = {
-        let state_guard = state.lock().unwrap();
-        if state_guard.is_some() {
+        let state_guard = state.lock().await;
+        if state_guard.client.is_some() {
             return Err("Light client is already running".to_string());
         }
         
-        client::LightClient::new()
+        EthereumClientBuilder::new()
+            .network(Network::MAINNET)
+            .consensus_rpc("https://www.lightclientdata.org")
+            .execution_rpc("https://eth-mainnet.g.alchemy.com/v2/")
+            .load_external_fallback()
+            .data_dir(PathBuf::from("/tmp/helios"))
+            .build()
             .map_err(|e| format!("Failed to create client: {}", e))?
     };
     
@@ -39,15 +51,33 @@ async fn start(state: tauri::State<'_, Mutex<Option<client::LightClient>>>) -> R
         .await
         .map_err(|e| format!("Failed to start client: {}", e))?;
     
-    client.wait_synced()
-        .await
-        .map_err(|e| format!("Failed to sync client: {}", e))?;
-
-    let mut state_guard = state.lock().unwrap();
-    if state_guard.is_some() {
-        return Err("Light client was started by another request".to_string());
+    client.wait_synced().await;
+    
+    {
+        let mut state_guard = state.lock().await;
+        state_guard.client = Some(client);
     }
-    *state_guard = Some(client);
 
     Ok("Light client started and synced successfully".to_string())
+}
+
+#[tauri::command]
+async fn get_block(state: tauri::State<'_, Mutex<AppState>>) -> Result<Option<Block<Transaction>>, String> {
+    let state_guard = state.lock().await;
+    let client = state_guard.client.as_ref()
+        .ok_or_else(|| "Light client not initialized".to_string())?;
+
+    client.get_block_by_number(BlockTag::Latest, false)
+        .await
+        .map_err(|e| format!("Failed to get latest block: {}", e))
+}
+
+struct AppState {
+    client: Option<EthereumClient<FileDB>>,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self { client: None }
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,27 @@
 
   let startMessage = $state<string>()
 
+  let block = $state<any>()
+
   const start = async () => {
     startMessage = await invoke('start')
   }
+
+  const getBlock = async () => {
+    block = await invoke('get_block')
+  }
+
+  $effect(() => {
+    if (!startMessage) return
+    const interval = setInterval(getBlock, 2000)
+
+    // Clean up effect
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
+
 </script>
 
 <h1>Mana</h1>
@@ -13,5 +31,9 @@
 {#if startMessage}
   <p>{startMessage}</p>
 {/if}
+{#if block}
+  <pre>{JSON.stringify(block, null, 2)}</pre>
+{/if}
+
 <p>Tauri <a href="https://v1.tauri.app/v1/guides/getting-started/setup/sveltekit">docs</a></p>
 <p>Sveltekit <a href="https://svelte.dev/docs/kit">docs</a></p>
